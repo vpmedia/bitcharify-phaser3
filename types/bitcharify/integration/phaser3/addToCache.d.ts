0dea4cee62555dd9fc3928567b1e504875bdaa55
Andras Csizmadia <andras@vpmedia.hu>
2023-04-17 11:04:39 +0200

    Added ts types

---
+++ types/bitcharify/integration/phaser3/addToCache.d.ts
@@ -0,0 +1,10 @@
+/**
+ * TBD
+ *
+ * @param {object} game TBD
+ * @param {string} key TBD
+ * @param {object} fontData TBD
+ * @param {HTMLImageElement|HTMLCanvasElement} textureSource TBD
+ */
+export function addToCache(game: object, key: string, fontData: object, textureSource: HTMLImageElement | HTMLCanvasElement): void;
+//# sourceMappingURL=addToCache.d.ts.map
---
+++ types/index.d.ts
@@ -0,0 +1,3 @@
+export { addToCache };
+import { addToCache } from "./bitcharify/integration/phaser3/addToCache";
+//# sourceMappingURL=index.d.ts.map
