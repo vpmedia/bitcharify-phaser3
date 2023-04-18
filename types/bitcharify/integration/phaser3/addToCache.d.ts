508d6b10a87cb372ae934f50ac262b849e2048a1
Andras Csizmadia <andras@vpmedia.hu>
2023-04-18 16:22:30 +0200

    Improve types

--- types/bitcharify/integration/phaser3/addToCache.d.ts
+++ types/bitcharify/integration/phaser3/addToCache.d.ts
@@ -1,10 +1,9 @@
 /**
- * TBD
- *
- * @param {object} game TBD
- * @param {string} key TBD
- * @param {object} fontData TBD
- * @param {HTMLImageElement|HTMLCanvasElement} textureSource TBD
+ * TBD.
+ * @param {object} game - TBD.
+ * @param {string} key - TBD.
+ * @param {object} fontData - TBD.
+ * @param {HTMLImageElement|HTMLCanvasElement} textureSource - TBD.
  */
 export function addToCache(game: object, key: string, fontData: object, textureSource: HTMLImageElement | HTMLCanvasElement): void;
 //# sourceMappingURL=addToCache.d.ts.map
