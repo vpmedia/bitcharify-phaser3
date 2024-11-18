f4e3d1022098adb2c4c4728e484a5e3b0d6d0fa7
Andras Csizmadia <andras@vpmedia.hu>
2024-11-18 11:37:57 +0100

    Disabled failing eslint unicorn rule

--- eslint.config.js
+++ eslint.config.js
@@ -51,6 +51,7 @@ export default [
       ...js.configs.recommended.rules,
       ...jsdocPlugin.configs['flat/recommended'].rules,
       ...unicornPlugin.configs['flat/recommended'].rules,
+      'unicorn/expiring-todo-comments': 'off',
       'unicorn/filename-case': 'warn',
       'unicorn/no-null': 'warn',
       'no-unused-vars': 'off',
